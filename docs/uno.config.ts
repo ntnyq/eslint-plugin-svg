3d2734d40de4837362c70410022bd658caefd735
ntnyq <ntnyq13@gmail.com>
2025-03-09 16:49:23 +0800

    chore: migrate pnpm settings

--- docs/uno.config.ts
+++ docs/uno.config.ts
@@ -1,12 +1,23 @@
-import { defineConfig, presetIcons, presetUno } from 'unocss'
+import {
+  defineConfig,
+  presetIcons,
+  presetWind3,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss'
 
 export default defineConfig({
+  shortcuts: [],
+  transformers: [transformerVariantGroup(), transformerDirectives()],
   presets: [
-    presetUno(),
+    presetWind3(),
     presetIcons({
       autoInstall: true,
       extraProperties: {},
       scale: 1.2,
     }),
   ],
+  theme: {
+    colors: {},
+  },
 })
